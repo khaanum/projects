570f29843decf7179f3c9138d81e1316249bd9c9
khaanum <numrotul64@gmail.com>
2017-11-30 20:05:33 +0700

    update

--- app/Http/Controllers/KomentarController.php
+++ app/Http/Controllers/KomentarController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\
 
 class KomentarController extends Controller
 {
     //
+    public function store(Request $request, Artikel $artikel)
+    {
+       // dd(request('message'));
+        Komentar::create([
+            'comment' => request('comment')
+        ]);
+    }
 }
--- app/Http/Controllers/WebartikelController.php
+++ app/Http/Controllers/WebartikelController.php
@@ -28,11 +28,5 @@ class WebartikelController extends Controller
         return view('website.artikel_single', compact('artikel'));
     }
 
-    public function store(Request $request, Artikel $artikel)
-    {
-       // dd(request('message'));
-        Komentar::create([
-            'comment' => request('comment')
-        ]);
-    }
+    
 }
